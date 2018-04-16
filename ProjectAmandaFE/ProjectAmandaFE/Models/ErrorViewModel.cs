e78ee111bceb37ac6e956bc655a1f34cb73db5c9
philipwerner <philip.r.werner@gmail.com>
2018-04-16 11:06:32 -0700

    completed initial file structure

---
+++ ProjectAmandaFE/ProjectAmandaFE/Models/ErrorViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectAmandaFE.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ProjectAmandaFE/ProjectAmandaFE/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectAmandaFE.Models
+{
+    public class User
+    {
+    }
+}
