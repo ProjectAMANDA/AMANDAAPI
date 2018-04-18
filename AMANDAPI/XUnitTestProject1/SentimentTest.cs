bafcdfea3e2296914e3ea0bf6e554358a0796a66
Playingtygre <kymiehsu123@gmail.com>
2018-04-18 11:30:56 -0700

    adding 1 test

--- AMANDAPI/XUnitTestProject1/SentimentTest.cs
+++ AMANDAPI/XUnitTestProject1/SentimentTest.cs
@@ -2,16 +2,39 @@ using System;
 using Xunit;
 using AMANDAPI.Data;
 using AMANDAPI.Models;
-using 
+using AMANDAPI.Controllers;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
 
 namespace XUnitTestProject1
 {
-    public class UnitTest1
+    public class ModelSentimentTest
     {
-        [Fact]
-        public void Test1()
+        ImagesContext _context;
+
+        public ModelSentimentTest()
         {
+            DbContextOptions<ImagesContext> options = new DbContextOptionsBuilder<ImagesContext>()
+              .UseInMemoryDatabase(Guid.NewGuid().ToString())
+              .Options;
+            _context = new ImagesContext(options);
+        }
+
+
+        [Fact]
+        public void GetId()
+        {
+            Image testsntiment = new Image()
+            {
+                Sentiment = "0.943"
+            };
+
+            Assert.Equal("0.943", testsntiment.Sentiment);
 
         }
+
+
+
+
     }
 }
