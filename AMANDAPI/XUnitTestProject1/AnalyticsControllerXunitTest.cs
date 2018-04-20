92c8bbebcb9fd63e8672431b3686ecaa21138b59
Playingtygre <kymiehsu123@gmail.com>
2018-04-20 09:47:29 -0700

    Testing only the analytics controller

--- AMANDAPI/XUnitTestProject1/AnalyticsControllerXunitTest.cs
+++ AMANDAPI/XUnitTestProject1/AnalyticsControllerXunitTest.cs
@@ -17,39 +17,13 @@ using System.Collections;
 
 namespace XUnitTestProject1
 {
-    //needs to touch Analyze string
-    // needs to touch Analytics controller
-    // needs to touch get text 
+  
     public class AnalyticsControllerXunitTest
     {
-        // This tests the Analyze method
-        /*
+        // This tests the Analyze method hits the GetText ; and part of the analytics.Analyze
+    
         [Fact]
-        public async void AnalyticsControllerXunitTest01()
-        {
-            var options = new DbContextOptionsBuilder<ImagesContext>()
-                .UseInMemoryDatabase(databaseName: "testDb")
-                .Options;
-
-            var builder = new ConfigurationBuilder().AddEnvironmentVariables();
-            builder.AddUserSecrets<Startup>();
-            var configuration = builder.Build();
-
-            using (var context = new ImagesContext(options))
-            {
-                var controller = new AnalyticsController(context, configuration);
-
-                //Act
-                var results = controller.Analyze("Little Baby foo foo");
-
-                //Assert
-                Assert.IsType<Analytics>(results);
-            }
-        }*/
-
-        
-        [Fact]
-        public async void AnalyticsControllerXunitTest02()
+        public async void AnalyticsControllerXunitGetText()
         {
             var options = new DbContextOptionsBuilder<ImagesContext>()
                 .UseInMemoryDatabase(databaseName: "testDb")
@@ -71,33 +45,6 @@ namespace XUnitTestProject1
             }
         }
         
-
-        /*
-        [Fact]
-        public async void AnalyticsControllerTest()
-        {
-            var options = new DbContextOptionsBuilder<ImagesContext>()
-                .UseInMemoryDatabase(databaseName: "testDb")
-                .Options;
-
-            var builder = new ConfigurationBuilder().AddEnvironmentVariables();
-            builder.AddUserSecrets<Startup>();
-            var configuration = builder.Build();
-
-            using (var context = new ImagesContext(options))
-            {
-                var controller = new AnalyticsController(context, configuration);
-
-                //Act
-                var results = controller.("keyphrases");
-                var results.
-
-                //Assert
-                Assert.IsNotType<Analytics>(results);
-            }
-        }*/
-
-
 
     }
 }
